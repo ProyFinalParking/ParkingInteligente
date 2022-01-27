4832f6eca0de89aaa1ed92565c587d9f4aa84d78
Agustin Rubio Chicote <agustin.rubio16@iesdoctorbalmis.com>
2022-01-27 12:21:13 +0100

    Prueba

--- ParkingInteligente/MainWindow.xaml.cs
+++ ParkingInteligente/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ParkingInteligente.mvvm;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,13 @@ namespace ParkingInteligente
     /// </summary>
     public partial class MainWindow : Window
     {
+        private MainWindowVM vm;
+
         public MainWindow()
         {
             InitializeComponent();
+            vm = new MainWindowVM();
+            this.DataContext = vm;
         }
     }
 }
