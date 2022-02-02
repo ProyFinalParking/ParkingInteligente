90240932dc0204b01f690c3492355d05782cfc08
Oleksandr Veretennykov <alc.platon@gmail.com>
2022-02-02 18:01:11 +0100

    Empiezo Ventana Gestión de estacionamientos

--- ParkingInteligente/mvvm/ControlAparcamientosVM.cs
+++ ParkingInteligente/mvvm/ControlAparcamientosVM.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using Microsoft.Toolkit.Mvvm.Messaging;
+using ParkingInteligente.modelo;
+using ParkingInteligente.servicios;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +13,44 @@ namespace ParkingInteligente.mvvm
 {
     class ControlAparcamientosVM : ObservableObject
     {
+        private readonly NavigationService servicio;
+
+        private Estacionamiento parkingSelect;
+        public Estacionamiento ParkingSelect
+        {
+            get { return parkingSelect; }
+            set { SetProperty(ref parkingSelect, value); }
+        }
+
+        private List<Estacionamiento> parkingList;
+        public List<Estacionamiento> ParkingList
+        {
+            get { return parkingList; }
+            set { SetProperty(ref parkingList, value); }
+        }
+
+        public RelayCommand CobrarFinalizarCommand { get; }
+
+        public ControlAparcamientosVM()
+        {
+            ParkingList = ServicioDB.GetListActivesParkedVehicles();
+            servicio = new NavigationService();
+            ParkingSelect = new Estacionamiento();
+
+            /*
+            WeakReferenceMessenger.Default.Register<ControlClientesVM, ClienteSeleccionadoRequestMessage>(this, (r, m) =>
+            {
+                m.Reply(r.ClienteSeleccionado);
+            });
+            */
+
+            CobrarFinalizarCommand = new RelayCommand(AbrirDialogoCobrarParking);
+        }
+
+        private void AbrirDialogoCobrarParking()
+        {
+            servicio.CargarDialogoAñadirCliente();
+        }
+
     }
 }
